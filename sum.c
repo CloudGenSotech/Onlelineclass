64e84bf076092474e645e0b6224502de320c90d7
ThotaThanish <sriurr@gmail.com>
2018-07-07 14:43:11 +0530

    sum.c

---
+++ sum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int a,b,c;
+	printf("\n Ente A B and C Values:");
+	scanf("%d%d%d",&a,&b,&c);
+	c=a+b+c;
+	printf("The sum of three values are %d",c);
+	getch();
+}
